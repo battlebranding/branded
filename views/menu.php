df33495acb918febc1a1fe72cf1bb9c55e72aeac
Marcus Battle <marcusbattle@Marcbook.local>
2015-11-25 09:13:33 -0500

    removed menu

--- views/menu.php
+++ views/menu.php
@@ -1,8 +1,8 @@
-<div class="header row">
+<!-- <div class="header row">
 	<div class="column col-md-4">
 		<h1 class="logo">Logo</h1>
 	</div>
 	<div class="column col-md-8">
 		Menu
 	</div>
-</div>	
+</div>	 -->
